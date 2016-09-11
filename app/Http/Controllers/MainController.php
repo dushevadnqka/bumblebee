4959f9d4a74d083d081ebb1fae96aa6d623c5b35
Georgi Aleksandrov <georgi.alexandroff@yahoo.com>
2016-09-12 01:22:47 +0300

    set config, front controller and bootstrapping the app

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MainController
+ *
+ * @author dushevadnqka
+ */
+class MainController
+{
+    public function index()
+    {
+        echo 'Ok, controller works!';
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,23 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
+/*
+  |--------------------------------------------------------------------------
+  | Create The Application
+  |--------------------------------------------------------------------------
+  |
+  | Here we will load the environment and create the application instance
+  | that serves as the central piece of this framework. We'll use this
+  | application as an "IoC" container and router for this framework.
+  |
+ */
+
 try {
     (new Dotenv\Dotenv(__DIR__.'/../'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
     throw new Exception($e->getMessage());
 }
+
+$app = \Core\App::getInstance();
+
+return $app;
