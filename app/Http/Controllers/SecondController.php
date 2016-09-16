a10ce1ba383989817c99e66c6b6369b38d259ced
Georgi Aleksandrov <georgi.alexandroff@yahoo.com>
2016-09-16 23:07:48 +0300

    test view

---
+++ app/Http/Controllers/SecondController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Description of SecondController
+ *
+ * @author dushevadnqka
+ */
+class SecondController
+{
+    public function index()
+    {
+        echo 'Second Controller index method';
+    }
+
+    public function another()
+    {
+        echo 'Second Controller another method';
+    }
+}
